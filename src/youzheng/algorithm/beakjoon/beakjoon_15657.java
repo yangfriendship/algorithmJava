52294cd24ce5f63ab5827649d75c3575cfe7d292
youzheng <yangfriendship@naver.com>
2020-10-19 11:56:32 +0900

    beakjoon_2529

--- src/youzheng/algorithm/beakjoon/beakjoon_15657.java
+++ src/youzheng/algorithm/beakjoon/beakjoon_15657.java
@@ -1,59 +1,48 @@
 package youzheng.algorithm.beakjoon;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class beakjoon_1182 {
-
-    static int cnt = 0;
+public class beakjoon_15657 {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        if (k == 0) {
-            cnt = -1;
-        }
 
         st = new StringTokenizer(br.readLine());
+
         int[] arr = new int[n];
+        boolean[] visit = new boolean[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        boolean[] visit = new boolean[n];
-        bfs(arr, visit, 0, 0, k, 0);
-        System.out.println(cnt);
+        Arrays.sort(arr);
+        bfs(arr, visit, 0, 0, k, "");
 
-    }
+        bw.flush();
+        ;
+        bw.close();
+
+
+    }//main
 
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static void bfs(int[] arr, boolean[] visit, int start, int r, int k, int sum) throws IOException {
+    private static void bfs(int[] arr, boolean[] visit, int start, int r, int k, String str) throws IOException {
 
-        if (k == sum) {
-
-            cnt++;
-        }
-        if (r >= arr.length) {
+        if (r == k) {
+            bw.write(str);
+            bw.newLine();
             return;
         }
-
         for (int i = start; i < arr.length; i++) {
-
-            if (!visit[i]) {
-                visit[i] = true;
-                bfs(arr, visit, i, r + 1, k, arr[i] + sum);
-                visit[i] = false;
-
-            }
-
+                bfs(arr, visit, i, r + 1, k, str + arr[i] + " ");
         }
-
     }
-
 
 }
---
+++ src/youzheng/algorithm/beakjoon/beakjoon_2529.java
@@ -0,0 +1,89 @@
+package youzheng.algorithm.beakjoon;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class beakjoon_2529 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = getArr();
+        String[] strArr = (br.readLine().replaceAll(" ","")).split("");
+        int k = strArr.length+1;
+        boolean[] visit = new boolean[10];
+        bfs(arr,visit,strArr,0,k,"");
+        Collections.sort(list);
+        System.out.println(list.get(list.size()-1));
+        System.out.println(list.get(0));
+
+
+    }//main
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static List<String> list = new ArrayList<>();
+    private static void bfs(int[] arr, boolean[] visit, String[] strArr, int r, int k, String str) throws IOException {
+
+        if (r  > k){
+            return;
+        }
+
+        if (r == k) {
+            if (isValid(str, strArr)) {
+
+                list.add(str);
+            }
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                bfs(arr, visit, strArr, r + 1, k, str + arr[i]);
+                visit[i] = false;
+            }
+
+
+        }
+
+    }
+
+    private static boolean isValid(String str, String[] strArr) {
+
+        for (int i = 0; i < str.length()-1; i++) {
+
+            int a = str.charAt(i)-'0';
+            int b = str.charAt(i + 1) - '0';
+
+            switch (strArr[i]){
+                case "<" :
+                    if (a>=b){
+                        return false;
+                    }
+
+                    break;
+                case ">" :
+                    if(a<=b){
+                        return  false;
+                    }
+
+                    break;
+            }
+        }
+
+
+        return true;
+    }
+
+    private static int[] getArr() {
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+}
