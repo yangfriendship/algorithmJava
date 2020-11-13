bdc44a811a6bc489d3ffb5f819c63a3e0e6fc4c9
youzheng <yangfriendship@naver.com>
2020-11-13 13:21:51 +0900

    beakjoon_3190-성공

--- src/youzheng/algorithm/beakjoon/beakjoon1/beakjoon_3190.java
+++ src/youzheng/algorithm/beakjoon/beakjoon1/beakjoon_3190.java
@@ -9,13 +9,13 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class beakjoon_3190 {
-    static int headX = 1;
+    static int headX = 1;   // 뱀은 배열 1,1에서 시작하므로 바로 초기화
     static int headY = 1;
-    static int direction = 1; //0 1 2 3 시계방향
-    static int n =0;
+    static int direction = 1; //0 위 , 1 오른쪽, 2 아래쪽 , 3, 왼쪽
+    static int n =0; 
     static int k =0;
-    static List<int[]> register = new LinkedList<>();
-    static  int[][] arr;
+    static List<int[]> register = new LinkedList<>();   // 뱀의 몸통의 x,y좌표 값을 저장하기 위해서 사용했다.
+    static  int[][] arr;                                // Game board
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,30 +43,33 @@ public class beakjoon_3190 {
             comms.add(new int[]{s,d});
         }
 
-        printMap(arr);
         int time = 0;
         register.add(new int[]{1,1});
         while(true){
 
-            if(time == comms.get(0)[0]){
-                if(comms.get(0)[1] == 1){
-                    turnRight();
-                }else if(comms.get(0)[1] == -1){
-                    turnLeft();
+
+            time ++;
+            move();
+            if(comms.size()>0){             //입력받은 명령을 담은 List에서 명령의시간과 현재 시간이 같다면 방향을 회정
+                if(time == comms.get(0)[0]){    //x초의 행동이 끝난 후, x초의 명령을 수행 -> 선 move() 후 방향전환
+                    if(comms.get(0)[1] == 1){
+                        comms.remove(0);
+                        turnRight();
+                    }else if(comms.get(0)[1] == -1){
+                        comms.remove(0);
+                        turnLeft();
+                    }
                 }
             }
 
-            move();
-            printSnake(arr);
-            time ++;
 
-            if (isOver()){
+            if (isOver()){      //게임종료 조건을 체크한다.
                 break;
             }
-            if (hasApple(arr)){ // 사과가 있음
-                eatApple();
+            if (hasApple(arr)){ // 사과가 있느냐?
+                eatApple(); //있다면 -> 꼬리는 건들지말고 머리부분만 몸통에 추가한다.
             }else {
-                notApple();
+                notApple(); //고리를 당겨준다
             }
 
 
@@ -93,6 +96,7 @@ public class beakjoon_3190 {
     }
     public static void notApple(){
         pullTail();
+        arr[headY][headX] = 2;
     }
 
     public static void pullTail(){
@@ -107,7 +111,7 @@ public class beakjoon_3190 {
             return;
         }else if(direction==2){
             toBottom();
-        }else if(direction==4){
+        }else if(direction==3){
             toLeft();
         }else if(direction==0){
             toTop();
@@ -184,8 +188,11 @@ public class beakjoon_3190 {
         for (int i = 1; i < arr.length; i++) {
 
             for (int j = 1; j < arr[0].length; j++) {
-
-                System.out.print(brr[i][j]);
+                if(arr[i][j]==1){
+                    System.out.print(2);
+                }else{
+                    System.out.print(brr[i][j]);
+                }
             }
             System.out.println();
 
