f76d764225413d8a36cba11cfb652459e520ca1b
youzheng <yangfriendship@naver.com>
2020-11-16 18:42:11 +0900

    programers42890- 비트마스킹 공부하고 다시 풀자..

---
+++ src/youzheng/algorithm/programers/level2/programers42890.java
@@ -0,0 +1,50 @@
+package youzheng.algorithm.programers.level2;
+
+public class programers42890 {
+
+    public static void main(String[] args) {
+
+        String[][] input = {{"100","ryan","music","2"}
+,{"200","apeach","math","2"}
+,{"300","tube","computer","3"}
+,{"400","con","computer","4"}
+,{"500","muzi","music","3"}
+,{"600","apeach","music","2"}};
+
+        solution(input);
+
+    }
+
+    public static int solution(String[][] relation) {
+        int answer = 0;
+
+        boolean[] isPk = new boolean[relation[0].length];
+        boolean[] visit = new boolean[isPk.length];
+        for (int i = 0; i < relation[0].length; i++) {
+
+            if()
+
+        }
+
+
+        return answer;
+    }
+
+    private static void bfs(String[][] relation, boolean[] isPk,boolean[] visit
+            , int r,int k, int start, String str) {
+
+
+
+
+    }
+
+    public static String getCombineStr(String[] arr,int start, int end){
+        StringBuffer sb = new StringBuffer();
+        for (int i = start; i <=end ; i++) {
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+
+
+}
